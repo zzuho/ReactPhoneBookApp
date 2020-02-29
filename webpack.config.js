d5d3098414fd07e7057b61b291eb2ba96788a7fe
zzuho <amazing28@gmail.com>
2020-02-29 12:30:41 +0900

    Initialize React Projext

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+'use strict'
+const path = require('path');
+
+module.exports ={
+    entry: {
+        main:['./src/main.js']
+    },
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            include: path.resolve(__dirname, './src'),
+            loaders: 'babel-loader'
+        }]
+    },
+    plugins: [],
+    devServer:{
+        contentBase: './public',
+        host: 'localhost',
+        port: 3000,
+        allowedHosts: [
+            'zzuho.asuscomm.com'
+        ],
+        hot: true
+    }
+}
